942e08c42b3ed149acea71f28948ad642879dadc
sehwaa <nsh235482@gmail.com>
2019-08-25 02:42:48 +0900

    인사봇 / 팀짜기 봇 추가

---
+++ glory_greeting_bot.py
@@ -0,0 +1,37 @@
+import asyncio
+import discord
+import logging
+
+client = discord.Client()
+
+#token
+token = 'NjEzNzE5ODk1MzA1MDkzMTY5.XV1B-w.CyK_LqSLkVPe7nh92hUhUQ0RwJk'
+
+#봇 구동
+@client.event
+async def on_ready():
+    logging.info("Logged in as")
+    logging.info("bot name : %s", client.user.name)
+    logging.info("bot id : %s", client.user.id)
+    
+    #봇 상태 출력
+    await client.change_presence(game=discord.Game(name='인사', type=1))
+
+@client.event
+async def on_member_join(member):
+    fmt = '''
+    {1.name}에 오신걸 환영합니다! {0.mention}님!
+1. 간단하게 자기소개 해주세요!
+Ex)나이/성별/별명/접속시간대/접률(일주일에접속횟수)등
+2. 닉네임은 저희랑 맞춰서 바꿔주시면 감사하겠습니다!
+3. 공지사항 한번씩 읽어주시는 것을 권장합니다!'''
+    channel = member.server.get_channel('612683932898689025')
+    await client.send_message(channel, fmt.format(member, member.server))
+
+@client.event
+async def on_member_remove(member):
+    channel = member.server.get_channel('612683932898689025')
+    fmt = '{0.mention}님이 서버에서 나가셨습니다!'
+    await client.send_message(channel, fmt.format(member, member.server))
+
+client.run(token)
---
+++ glory_team.py
@@ -0,0 +1,73 @@
+from discord.ext import commands
+import discord
+import random
+ 
+token = 'NjEzMDAyMzAxODYyNTc2MTU5.XVql_g.n-sea8ofjjoCJGtTMw6s33jYjtY'
+
+bot = commands.Bot(command_prefix='$')
+ 
+@bot.event
+async def on_ready():
+    print('logged in as \nname: {}\n  id: {}'.format(bot.user.name, bot.user.id))
+    print('='*80)
+    
+    await bot.change_presence(game=discord.Game(name='팀짜기', type=1))
+ 
+@bot.command(pass_context=True)
+async def divide_team(ctx, count):
+    voice_channel = ctx.message.author.voice.voice_channel
+
+    members = voice_channel.voice_members
+    member_names = []
+    for member in members:
+        member_names.append(member.mention)
+    random.shuffle(member_names)
+    
+    team = []
+
+    for i in range(0, int(len(member_names)/int(count))):
+        temp = []
+        for c in range(0, int(count)):
+            temp.append(member_names.pop())
+        team.append(temp)
+
+    if member_names:
+        team.append(member_names)
+
+    for index in range(0, len(team)):
+        await bot.say('{} team : {}'.format(index+1, team[index]))
+
+@bot.command(pass_context=True)
+async def team_except(ctx, count, *args):
+    voice_channel = ctx.message.author.voice.voice_channel
+
+    members = voice_channel.voice_members
+    member_names = []
+
+    __MEMBER__ = {}
+    
+    for member in members:
+        __MEMBER__[member.nick] = True
+
+    for except_member in args:
+        __MEMBER__[except_member] = False
+
+    for member in members:
+        if __MEMBER__[member.nick] != False:
+            member_names.append(member.mention)
+    random.shuffle(member_names)
+    
+    team = []
+
+    for i in range(0, int(len(member_names)/int(count))):
+        temp = []
+        for c in range(0, int(count)):
+            temp.append(member_names.pop())
+        team.append(temp)
+
+    if member_names:
+        team.append(member_names)
+
+    for index in range(0, len(team)):
+        await bot.say('{} team : {}'.format(index+1, team[index]))
+bot.run(token)
