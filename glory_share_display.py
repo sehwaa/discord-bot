db8f563e2e3c901e09339a991160fde7c1f1665c
sehwaa <nsh235482@gmail.com>
2019-09-02 00:54:53 +0900

    add share display

---
+++ glory_share_display.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+import discord
+import random
+ 
+token = 'Your Token'
+
+bot = commands.Bot(command_prefix='$')
+ 
+@bot.event
+async def on_ready():
+    print('logged in as \nname: {}\n  id: {}'.format(bot.user.name, bot.user.id))
+    print('='*80)
+    
+    await bot.change_presence(game=discord.Game(name='팀짜기', type=1))
+ 
+@bot.command(pass_context=True)
+async def share(ctx):
+    voice_channel = ctx.message.author.voice.voice_channel
+
+    voice_channel_id = voice_channel.id
+
+    url = 'https://discordapp.com/channels/611294722501378048/'+str(voice_channel_id)
+
+    await bot.say('{}'.format(url))
